935fe3e7318fa32e392b3ddf31d742f5b2852a50
alfazsozib <alfazsozib.com>
2024-04-08 16:25:52 +0600

    first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from "react";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-     
+     <Home />
     </div>
   );
 }
